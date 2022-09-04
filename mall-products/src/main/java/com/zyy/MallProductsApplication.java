cad5ccd5003ec7a3b4bc0254d7ba841d5e07d339
zooeyace <zooeyace66@gmail.com>
2022-09-04 17:10:10 +0800

    初始化

---
+++ mall-products/src/main/java/com/zyy/MallProductsApplication.java
@@ -0,0 +1,13 @@
+package com.zyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallProductsApplication.class, args);
+    }
+
+}
